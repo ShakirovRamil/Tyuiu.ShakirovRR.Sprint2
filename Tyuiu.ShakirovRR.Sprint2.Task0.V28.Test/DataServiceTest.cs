0b99985fd687008e8a30b88e250805b5c27eb974
Ramil Shakirov <ram.shok@bk.ru>
2023-10-03 20:37:27 +0500

    Add project files.

---
+++ Tyuiu.ShakirovRR.Sprint2.Task0.V28.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ShakirovRR.Sprint2.Task0.V28.Lib;
+
+namespace Tyuiu.ShakirovRR.Sprint2.Task0.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void validGetCompareOperations()
+        {
+            DataService ds = new DataService();
+            int x = 111;
+            int y = 735;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] {false, false, false, false, false, false};
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ShakirovRR.Sprint2.Task0.V28/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ShakirovRR.Sprint2.Task0.V28.Lib;
+
+namespace Tyuiu.ShakirovRR.Sprint2.Task0.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Шакиров Р. Р. | ИСТНБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Базовые навки работы в C#                                         *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил: Шакиров Рамиль Русланович | ИСТНБ-23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* и выводит на экран результат данного нам уравнения.                     *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x = 111;
+            int y = 735;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+
+            Console.WriteLine("X = " + x);
+            Console.WriteLine("Y = " + y);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
