8381a26a2bf552588553718d6dbb9fa112827548
Ramil Shakirov <ram.shok@bk.ru>
2023-10-16 20:47:29 +0500

    Task7

--- Tyuiu.ShakirovRR.Sprint2.Task5.V2.Test/DataServiceTest.cs
+++ Tyuiu.ShakirovRR.Sprint2.Task5.V2.Test/DataServiceTest.cs
@@ -11,6 +11,29 @@ namespace Tyuiu.ShakirovRR.Sprint2.Task5.V2.Test
         [TestMethod]
         public void TestMethod1()
         {
+            DataService ds = new DataService();
+
+            Assert.AreEqual("Зима", ds.FindMonthSeason(12));
+            Assert.AreEqual("Зима", ds.FindMonthSeason(1));
+            Assert.AreEqual("Зима", ds.FindMonthSeason(2));
+            Assert.AreEqual("Весна", ds.FindMonthSeason(3));
+            Assert.AreEqual("Весна", ds.FindMonthSeason(4));
+            Assert.AreEqual("Весна", ds.FindMonthSeason(5));
+            Assert.AreEqual("Лето", ds.FindMonthSeason(6));
+            Assert.AreEqual("Лето", ds.FindMonthSeason(7));
+            Assert.AreEqual("Лето", ds.FindMonthSeason(8));
+            Assert.AreEqual("Осень", ds.FindMonthSeason(9));
+            Assert.AreEqual("Осень", ds.FindMonthSeason(10));
+            Assert.AreEqual("Осень", ds.FindMonthSeason(11));
+
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindMonthSeason(-1);
+            });
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindMonthSeason(13);
+            });
         }
     }
 }
--- Tyuiu.ShakirovRR.Sprint2.Task5.V2/Program.cs
+++ Tyuiu.ShakirovRR.Sprint2.Task5.V2/Program.cs
@@ -12,6 +12,45 @@ namespace Tyuiu.ShakirovRR.Sprint2.Task5.V2
     {
         static void Main(string[] args)
         {
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт #2 | Выполнил: Шакиров Р. Р. | ИСТНБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Базовые навки работы в C#                                         *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Шакиров Рамиль Русланович | ИСТНБ-23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая выводит на экран результат                  *");
+            Console.WriteLine("* данного нам задания                                                     *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите номер месяца: ");
+            int numMonth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение Y");
+            string res;
+            if ((numMonth < 1) || (numMonth > 12))
+            {
+                res = "Введены не верные значения";
+
+            }
+            else
+            {
+                res = "Это время года:" + ds.FindMonthSeason(numMonth);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+
+            Console.ReadLine();
         }
     }
 }
